732aeda0f3418f7392d96bff733b040653d6f263
agkden <agkden@yahoo.com>
2021-03-31 11:58:34 -0600

    update file
    demographic info added

--- Belly-Button-Biodiversity/static/js/app.js
+++ Belly-Button-Biodiversity/static/js/app.js
@@ -17,7 +17,11 @@ function dropDownMenu() {
     // create dropdown menu
     subjectIds.forEach((individ) => {
       dropdownId.append("option").text(individ).property("value", individ);      
-    });       
+    });    
+
+
+    // initialize the dashboard with the first sample
+    demographicInfo(0);    
 
   });
 }
@@ -45,9 +49,28 @@ function dropDownMenu() {
 
 
 
-// 4-5. Display each key-value pair (individual's demographic information) from the metadata JSON object for the sample in dropdown menu.
+// 4-5. Display each key-value pair (individual's demographic information) from the metadata JSON object for the sample selected in the dropdown menu.
 
+function demographicInfo(sampleIdx) {
+  
+  d3.json("data/samples.json").then((importedData) => {
+    var demographics = importedData.metadata[sampleIdx];
+    console.log(demographics);
 
+    // use d3 to select 'Demographic Info' panel by id
+    infoPanel = d3.select("#sample-metadata");
+
+    // clear any previous input
+    infoPanel.html("");
+
+    // add each key-value pair to the panel
+    Object.entries(demographics).forEach(([key, value]) => {
+      infoPanel.append("p").text(`${key}: ${value}`);
+    });
+
+  });
+
+}
 
 
 // 6. Update all of the plots any time that a new sample is selected.
